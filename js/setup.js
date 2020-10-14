f5df04a288a9127cc71c351e4cfa07e8a1d4e1c4
Eric Alberto Costa e Costa <71726316+ericcostac@users.noreply.github.com>
2020-10-14 10:13:47 -0300

    Js incrementado

--- js/setup.js
+++ js/setup.js
@@ -152,9 +152,17 @@ function start() {
 
     function colisao() {
         var colisao1 = ($("#jogador").collision($("#inimigo1")));
-        // jogador com o inimigo1
-    
-        console.log(colisao1);
-    
-    }
+            
+            if (colisao1.length>0) {
+                
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X,inimigo1Y);
+        
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+            }
+        
+        }
  }
